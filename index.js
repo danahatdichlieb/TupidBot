06796e6ae23f0c8801aec0e8f3bb7c44e38cc3e5
danahatdichlieb <xdanaatv@gmail.com>
2025-03-31 16:27:53 +0200

    hi

--- index.js
+++ index.js
@@ -1,49 +1,27 @@
+import { ChatClient } from "@mastondzn/dank-twitch-irc";
+import { loadCommands } from "./utils/loadCommands.js";
+import { handleCommand } from "./utils/handleCommand.js";
 import fs from "fs";
 
 const config = JSON.parse(await fs.promises.readFile("config.json", "utf8"));
+const PREFIX = "+";
 
-import { ChatClient } from "@mastondzn/dank-twitch-irc";
-
-const USERNAME = config.USERNAME;
-const PASSWORD = config.PASSWORD;
 const chat = new ChatClient({
-    username: USERNAME,
-    password: PASSWORD,
-    rateLimits: "default",
-    connection: { type: "websocket", secure: true },
-    requestMembershipCapability: false,
-    ignoreUnhandledPromiseRejections: true
+    username: config.USERNAME,
+    password: config.PASSWORD,
+    connection: { type: "websocket", secure: true }
 });
 
-chat.on("ready", () => console.log("danaabot ready!"));
-chat.on("close", (error) => {
-    if (error) {
-        console.error("❌ Fehler:", error);
-    }
-});
-chat.on("PRIVMSG", async (msg) => {
-    console.log(`[#${msg.channelName}] ${msg.displayName}: ${msg.messageText}`);
+const commands = await loadCommands();
 
-    if (msg.displayName.toLowerCase() !== USERNAME.toLowerCase()) {
+chat.on("ready", () => console.log("TupidBot joined!"));
+chat.on("close", (error) => error && console.error("❌ Fehler:", error));
 
-        if (msg.messageText === "hi"){
-            chat.say(msg.channelName, "hi);
-        }
-        if (msg.messageText === "Olrite"){
-            chat.say(msg.channelName, "Olritey ");
-        }
-        if (msg.messageText === "why"){
-            chat.say(msg.channelName, "why not ");
-        }
-    }
-});
+chat.on("PRIVMSG", async (msg) => handleCommand(chat, msg, commands, PREFIX));
 
 async function startBot() {
     await chat.connect();
-
-    setTimeout(() => {
-        chat.join("danahatdichlieb", "danahatdichlieb");
-    }, 2000);
+    ["#tupidbot", "#jonasdirektschaltgetriebe", "#danahatdichlieb"].forEach(channel => chat.join(channel));
 }
 
 startBot();
