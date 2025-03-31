701dd20a8f98783098483ba16b2f2c8888b58318
danahatdichlieb <xdanaatv@gmail.com>
2025-03-31 17:20:49 +0200

    update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -4,8 +4,14 @@ export function timeSince(timestamp) {
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
 
-    if (days > 0) return `${days} Tag(e), ${hours % 24} Stunden`;
-    if (hours > 0) return `${hours} Stunde(n), ${minutes % 60} Minuten`;
-    if (minutes > 0) return `${minutes} Minute(n), ${seconds % 60} Sekunden`;
+    if (days > 0) {
+        return `${days} ${days === 1 ? 'Tag' : 'Tage'}, ${hours % 24} Stunde(n)`;
+    }
+    if (hours > 0) {
+        return `${hours} ${hours === 1 ? 'Stunde' : 'Stunden'}, ${minutes % 60} Minute(n)`;
+    }
+    if (minutes > 0) {
+        return `${minutes} ${minutes === 1 ? 'Minute' : 'Minuten'}, ${seconds % 60} Sekunden`;
+    }
     return `${seconds} Sekunden`;
 }
