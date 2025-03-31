64b5a1a95d814326b8ca9b1f1ba64ef37c3458da
danahatdichlieb <xdanaatv@gmail.com>
2025-03-31 21:05:36 +0200

    update hi.js

--- commands/hi.js
+++ commands/hi.js
@@ -1,6 +1,6 @@
 export default {
     name: "hi",
-    aliases: ["hi"],
+    aliases: ["hii"],
     description: "hi",
     async execute(chat, msg) {
         chat.say(msg.channelName, `@${msg.displayName} hi`);
