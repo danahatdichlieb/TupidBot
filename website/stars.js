88e3c9620c788a7ad502aa5dcc84ca7c6c34e3dd
danahatdichlieb <xdanaatv@gmail.com>
2025-03-31 21:43:06 +0200

    add website

---
+++ website/stars.js
@@ -0,0 +1,35 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const starsContainer = document.querySelector('.stars');
+
+    function createStars(count) {
+        for (let i = 0; i < count; i++) {
+            let star = document.createElement("div");
+            star.classList.add("star");
+
+
+            let x = Math.random() * window.innerWidth;
+            let y = Math.random() * window.innerHeight;
+
+
+            let size = Math.random() * 3 + 1;
+
+
+            let duration = Math.random() * 6 + 1;
+            let delay = Math.random() * 2;
+
+
+            star.style.left = `${x}px`;
+            star.style.top = `${y}px`;
+            star.style.width = `${size}px`;
+            star.style.height = `${size}px`;
+            star.style.animationDuration = `${duration}s`;
+            star.style.animationDelay = `-${delay}s`;
+
+            starsContainer.appendChild(star);
+        }
+    }
+
+    createStars(50);
+});
+
