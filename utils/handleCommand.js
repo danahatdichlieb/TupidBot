6b0118410a9349dc22b6c21e930d7082ae227d9a
danahatdichlieb <xdanaatv@gmail.com>
2025-04-01 15:58:44 +0200

    add cooldowns

--- utils/handleCommand.js
+++ utils/handleCommand.js
@@ -1,10 +1,23 @@
+import { bot } from './bot.js';
+
 export async function handleCommand(chat, msg, commands, PREFIX) {
     if (!msg.messageText.startsWith(PREFIX)) return;
 
     const args = msg.messageText.slice(PREFIX.length).trim().split(" ");
-    const command = commands.get(args.shift().toLowerCase());
+    const commandName = args.shift().toLowerCase();
+    const command = commands.get(commandName);
 
     if (command) {
+        const user = msg.username;
+
+        // Cooldown-Check
+        if (bot.cooldowns.has(user)) {
+            console.log("COOLDOWN!!!")
+            return;
+        }
+
+        bot.cooldowns.set(user, bot.cooldowns.durations.short);
+
         try {
             await command.execute(chat, msg, args);
         } catch (error) {
