581078956cd8112320a658e563156f75f6ee3116
danahatdichlieb <xdanaatv@gmail.com>
2025-04-02 23:31:46 +0200

    update website & add commands.js

---
+++ commands/commands.js
@@ -0,0 +1,9 @@
+export default {
+    name: "commands",
+    aliases: ["Link zur TupidBot Website:"],
+    description: "hi",
+    cooldown: 5,
+    async execute(chat, msg) {
+        return { text: `DA https://tupidbot.vercel.app/` };
+    }
+};
