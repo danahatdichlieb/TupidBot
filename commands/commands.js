09085783d293cc2bac550238c1014310c8c9f290
danahatdichlieb <xdanaatv@gmail.com>
2025-04-02 23:37:27 +0200

    update website & add commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -1,7 +1,7 @@
 export default {
     name: "commands",
-    aliases: ["Link zur TupidBot Website"],
-    description: "hi",
+    aliases: ["cmd"],
+    description: "Link zur TupidBot Website",
     cooldown: 5,
     async execute(chat, msg) {
         return { text: `DA https://tupidbot.vercel.app/` };
--- commands/hi.js
+++ commands/hi.js
@@ -1,7 +1,7 @@
 export default {
     name: "hi",
-    aliases: ["cmd"],
-    description: "hi",
+    aliases: [""],
+    description: "TupidBot sagt hi",
     cooldown: 5,
     async execute(chat, msg) {
         return { text: `hi` };
