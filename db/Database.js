b751336ae2f0367f6d45ba9df111ff09b040405f
danahatdichlieb <xdanaatv@gmail.com>
2025-04-11 12:39:42 +0200

    add command liste.js

---
+++ db/Database.js
@@ -0,0 +1,40 @@
+import { createPool } from 'mariadb';
+import db from './db.js';
+
+export class Database {
+    constructor() {
+        this.pool = createPool({
+            user: db.db.user,
+            password: db.db.pass,
+            database: db.db.name,
+            host: db.db.host
+        });
+    }
+
+    async query(queryParam, params = []) {
+        let connection;
+        let result;
+
+        try {
+            connection = await this.pool.getConnection();
+            result = await connection.query(queryParam, params);
+        } catch (e) {
+            console.error('[Database Error]', e);
+
+        } finally {
+            connection?.release();
+        }
+
+        return result || [];
+    }
+
+    async queryOne(queryStr, params = [], addLimit = true) {
+        const result = await this.query(`${queryStr}${addLimit ? ' LIMIT 1' : ''}`, params);
+        return result?.[0] || false;
+    }
+
+    async entryExists(queryStr, params = [], addLimit = true) {
+        const rows = await this.query(`${queryStr}${addLimit ? ' LIMIT 1' : ''}`, params);
+        return rows.length > 0;
+    }
+}
