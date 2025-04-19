e398c01ce1b056be16c5ac2df9c28b423f023cd6
danahatdichlieb <xdanaatv@gmail.com>
2025-04-19 16:30:37 +0200

    add add.js

--- utils/bot.js
+++ utils/bot.js
@@ -1,11 +1,11 @@
-import { channels } from "./channels.js";
+import Database from '../db/Database.js';
 import { Cooldown } from "./cooldown.js";
 
 export const bot = {
   channels: {
-    channelsList: channels,
-    getAll() {
-      return this.channelsList;
+    async getAll() {
+      const db = new Database();
+      return await db.query("SELECT name FROM channels");
     },
   },
   stats: { runningSince: Date.now() },
