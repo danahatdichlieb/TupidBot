26cf8b3e1a9293691b9b1342a5d9e2d045923f02
danahatdichlieb <xdanaatv@gmail.com>
2025-05-02 17:20:13 +0200

    add command randomstream.js

---
+++ commands/randomstream.js
@@ -0,0 +1,43 @@
+import humanizeDuration from '../utils/humanizeDuration.js';
+import { GetFirstStreams, GetTopGames } from '../utils/twitch.js';
+
+export default {
+    name: "randomstream",
+    aliases: ["rs"],
+    description: "Gibt einen random Stream",
+    cooldown: 5,
+    permission: 0,
+    async execute(chat, msg, args) {
+        const topGames = await GetTopGames(100);
+
+        if (!topGames || topGames.length === 0) {
+            console.log('Fehler: Keine Top-Spiele gefunden');
+            return {
+                text: "Es konnte kein Spiel gefunden werden. Versuche es später nochmal.",
+                reply: true,
+            };
+        }
+
+        const getRandomGame = topGames[Math.floor(Math.random() * topGames.length)];
+        const data2 = await GetFirstStreams(100, getRandomGame['id']);
+
+        if (!data2 || data2.length === 0) {
+            console.log('Fehler: Keine Streams für das ausgewählte Spiel gefunden');
+            return {
+                text: "Es konnte kein Stream für dieses Spiel gefunden werden.",
+                reply: true,
+            };
+        }
+
+        const random = data2[Math.floor(Math.random() * data2.length)];
+        const { user_name, started_at, game_name, viewer_count, user_login, title } = random;
+        const ms = new Date().getTime() - Date.parse(started_at);
+
+        const shortenedTitle = title.length > 30 ? title.slice(0, 30) + '...' : title;
+
+        return {
+            text: `ApuApustaja @${user_name} ist seit ${humanizeDuration(ms, 2)} live | Spielt ${game_name} mit ${viewer_count} Zuschauern | Titel: ${shortenedTitle} | twitch.tv/${user_login} |`,
+            reply: true,
+        };
+    },
+};
--- utils/twitch.js
+++ utils/twitch.js
@@ -1,4 +1,32 @@
 import got from 'got';
+import fs from "fs";
+
+let config = null;
+
+async function loadConfig() {
+    if (!config) {
+        const file = await fs.promises.readFile('./config.json', 'utf8');
+        config = JSON.parse(file);
+    }
+    return config;
+}
+
+let helix = null;
+
+async function createHelix() {
+    const config = await loadConfig();
+    helix = got.extend({
+        prefixUrl: 'https://api.twitch.tv/helix/',
+        throwHttpErrors: false,
+        responseType: 'json',
+        headers: {
+            'Client-ID': config.client_id,
+            Authorization: `Bearer ${config.oauth}`,
+        },
+    });
+}
+
+createHelix();
 
 export const IDByLogin = async (username) => {
     if (!username) return null;
@@ -6,8 +34,7 @@ export const IDByLogin = async (username) => {
         responseType: 'json',
         throwHttpErrors: false,
         headers: {
-            'User-Agent':
-                'Forsen.',
+            'User-Agent': 'Forsen.',
         },
     });
     if (!body[0]?.id || body?.length === 0 || body?.error) return null;
@@ -15,6 +42,23 @@ export const IDByLogin = async (username) => {
 };
 
 export const ParseUser = (user) => {
-    const parsed = user.replace(/[@#,]/g, ''); // Remove @, #, and ,
+    const parsed = user.replace(/[@#,]/g, '');
     return parsed.toLowerCase();
 };
+
+export const GetFirstStreams = async (number, game) => {
+    if (!helix) await createHelix();
+    const { data } = await helix.get(`streams?first=${number}&game_id=${game}`).json();
+    return data;
+};
+
+export const GetTopGames = async (amount) => {
+    try {
+        const response = await helix.get(`games/top?first=${amount}`).json();
+        return response.data;
+    } catch (error) {
+        return [];
+    }
+};
+
+
